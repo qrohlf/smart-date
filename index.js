6f82b8d24ab9dfd55c2f07d96262b7d8c006c6f5
Quinn Rohlf <qr@qrohlf.com>
2016-06-30 16:48:12 -0700

    1.0.0

---
+++ index.js
@@ -0,0 +1,63 @@
+const moment = require('moment')
+
+function smartDate (dates, options) {
+  if (!Array.isArray(dates)) { dates = [dates] }
+  if (dates.length === 1) {
+    return formatSingleDate(dates[0], options)
+  } else if (dates.length === 2) {
+    return formatDateRange(...dates, options)
+  } else {
+    throw TypeError('smartDate only accepts 0-2 dates')
+  }
+}
+
+function applyDefaults (date, options) {
+  const defaults = {
+    includeTime: date.includes(' ') || date.includes('T'),
+    includeYear: moment(date).year() !== moment().year()
+  }
+  return Object.assign({}, defaults, options)
+}
+
+function formatSingleDate (date, options) {
+  const {includeTime, includeYear} = applyDefaults(date, options)
+  const m = moment(date)
+  const format = [
+    'MMMM Do',
+    includeTime && 'h:mm a',
+    includeYear && 'YYYY'
+  ].filter(f => f).join(', ')
+  return m.format(format)
+}
+
+function formatDateRange (start, end, options) {
+  const m1 = moment(start)
+  const m2 = moment(end)
+
+  // only include the year in the first date if it doesn't match the second date
+  const _startOpts = Object.assign({}, options, {includeYear: m1.year() !== m2.year()})
+  const startOpts = applyDefaults(start, _startOpts)
+  const endOpts = applyDefaults(end, options)
+  const anyTimes = startOpts.includeTime || endOpts.includeTime
+
+  // If the dates are identical, return one date
+  if (m1.isSame(m2)) {
+    return formatSingleDate(start, options)
+  }
+
+  // If the dates are in the same month,
+  if (m1.isSame(m2, 'month') && !anyTimes) {
+    const endFormat = [
+      'D',
+      endOpts.includeTime && 'h:mm a',
+      endOpts.includeYear && 'YYYY'
+    ].filter(f => f).join(', ')
+    return `${m1.format('MMMM D')}-${m2.format(endFormat)}`
+  }
+
+  return `${formatSingleDate(start, startOpts)} - ${formatSingleDate(end, endOpts)}`
+}
+
+module.exports = smartDate
+module.exports.formatSingleDate = formatSingleDate
+module.exports.formatDateRange = formatDateRange
---
+++ tests.js
@@ -0,0 +1,173 @@
+const {expect} = require('chai')
+const sinon = require('sinon')
+const smartDate = require('./index')
+const {formatSingleDate, formatDateRange} = smartDate
+
+const TODAY = '2016-06-30' // may not actually be today tomorrow
+
+describe('formatSingleDate', () => {
+  let clock
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(new Date(TODAY).getTime())
+  })
+
+  afterEach(() => {
+    clock.restore()
+  })
+
+  it('one date, this year', () => {
+    expect(formatSingleDate('2016-07-04')).to.eql('July 4th')
+  })
+
+  it('one date, next year', () => {
+    expect(formatSingleDate('2017-07-04')).to.eql('July 4th, 2017')
+  })
+
+  it('one date, with time', () => {
+    expect(formatSingleDate('2016-07-04 09:30')).to.eql('July 4th, 9:30 am')
+  })
+
+  it('one date, next year, with time', () => {
+    expect(formatSingleDate('2017-07-04 09:30')).to.eql('July 4th, 9:30 am, 2017')
+  })
+
+  it('respects the year override', () => {
+    expect(formatSingleDate('2016-07-04')).to.eql('July 4th')
+    expect(formatSingleDate('2016-07-04', {includeYear: true})).to.eql('July 4th, 2016')
+
+    expect(formatSingleDate('2017-07-04')).to.eql('July 4th, 2017')
+    expect(formatSingleDate('2017-07-04', {includeYear: false})).to.eql('July 4th')
+  })
+
+  it('respects the time override', () => {
+    expect(formatSingleDate('2016-07-04 09:30')).to.eql('July 4th, 9:30 am')
+    expect(formatSingleDate('2016-07-04 09:30', {includeTime: false})).to.eql('July 4th')
+
+    expect(formatSingleDate('2016-07-04')).to.eql('July 4th')
+    expect(formatSingleDate('2016-07-04', {includeTime: true})).to.eql('July 4th, 12:00 am')
+  })
+})
+
+describe('formatDateRange', () => {
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(new Date(TODAY).getTime())
+  })
+
+  afterEach(() => {
+    clock.restore()
+  })
+
+  it('equal day, equal month, equal year, no times', () => {
+    expect(formatDateRange('2016-07-04', '2016-07-04')).to.eql('July 4th')
+  })
+
+  it('equal day, equal month, equal year, no times, next year', () => {
+    expect(formatDateRange('2017-07-04', '2017-07-04')).to.eql('July 4th, 2017')
+  })
+
+  it('equal month, equal year, no times', () => {
+    expect(formatDateRange('2016-07-04', '2016-07-05')).to.eql('July 4-5')
+  })
+
+  it('equal month, equal year, no times, next year', () => {
+    expect(formatDateRange('2017-07-04', '2017-07-05')).to.eql('July 4-5, 2017')
+  })
+
+  it('equal month, equal year, start time', () => {
+    expect(formatDateRange('2016-07-04 09:30', '2016-07-05')).to.eql('July 4th, 9:30 am - July 5th')
+  })
+
+  it('equal month, equal year, start time, next year', () => {
+    expect(formatDateRange('2017-07-04 09:30', '2017-07-05')).to.eql('July 4th, 9:30 am - July 5th, 2017')
+  })
+
+  it('equal month, equal year, end time', () => {
+    expect(formatDateRange('2016-07-04', '2016-07-05 09:30')).to.eql('July 4th - July 5th, 9:30 am')
+  })
+
+  it('equal month, equal year, end time, next year', () => {
+    expect(formatDateRange('2017-07-04', '2017-07-05 09:30')).to.eql('July 4th - July 5th, 9:30 am, 2017')
+  })
+
+  it('different month, equal year', () => {
+    expect(formatDateRange('2016-07-04', '2016-08-05')).to.eql('July 4th - August 5th')
+  })
+
+  it('different month, equal year, next year', () => {
+    expect(formatDateRange('2017-07-04', '2017-08-05')).to.eql('July 4th - August 5th, 2017')
+  })
+
+  it('different month, equal year, start time', () => {
+    expect(formatDateRange('2016-07-04 09:30', '2016-08-05')).to.eql('July 4th, 9:30 am - August 5th')
+  })
+
+  it('different month, equal year, start time, next year', () => {
+    expect(formatDateRange('2017-07-04 09:30', '2017-08-05')).to.eql('July 4th, 9:30 am - August 5th, 2017')
+  })
+
+  it('different month, equal year, end time', () => {
+    expect(formatDateRange('2016-07-04', '2016-08-05 09:30')).to.eql('July 4th - August 5th, 9:30 am')
+  })
+
+  it('different month, equal year, end time, next year', () => {
+    expect(formatDateRange('2017-07-04', '2017-08-05 09:30')).to.eql('July 4th - August 5th, 9:30 am, 2017')
+  })
+
+  it('different year', () => {
+    expect(formatDateRange('2016-07-04', '2017-07-04')).to.eql('July 4th, 2016 - July 4th, 2017')
+  })
+
+  it('different year, start time', () => {
+    expect(formatDateRange('2016-07-04 09:30', '2017-07-05')).to.eql('July 4th, 9:30 am, 2016 - July 5th, 2017')
+  })
+
+  it('different year, end time', () => {
+    expect(formatDateRange('2016-07-04', '2017-07-05 09:30')).to.eql('July 4th, 2016 - July 5th, 9:30 am, 2017')
+  })
+
+  it('respects the time override', () => {
+    expect(formatDateRange('2016-07-04', '2016-07-05')).to.eql('July 4-5')
+    expect(formatDateRange('2016-07-04', '2016-07-05', {includeTime: true})).to.eql('July 4th, 12:00 am - July 5th, 12:00 am')
+  })
+
+  it('respects the year override', () => {
+    expect(formatDateRange('2016-07-04', '2016-07-05')).to.eql('July 4-5')
+    expect(formatDateRange('2016-07-04', '2016-07-05', {includeYear: true})).to.eql('July 4-5, 2016')
+  })
+})
+
+describe('smartDate', () => {
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(new Date(TODAY).getTime())
+  })
+
+  afterEach(() => {
+    clock.restore()
+  })
+
+  it('accepts a single date string', () => {
+    expect(smartDate('2016-07-04')).to.eql('July 4th')
+  })
+
+  it('passes through options for single dates', () => {
+    expect(smartDate('2016-07-04')).to.eql('July 4th')
+    expect(smartDate('2016-07-04', {includeTime: true})).to.eql('July 4th, 12:00 am')
+    expect(smartDate('2016-07-04', {includeYear: true})).to.eql('July 4th, 2016')
+  })
+
+  it('accepts a single date array', () => {
+    expect(smartDate(['2016-07-04'])).to.eql('July 4th')
+  })
+
+  it('accepts a date range', () => {
+    expect(smartDate(['2016-07-04 09:30', '2016-07-05'])).to.eql('July 4th, 9:30 am - July 5th')
+  })
+
+  it('passes through options for date ranges', () => {
+    expect(smartDate(['2016-07-04', '2016-07-05'])).to.eql('July 4-5')
+    expect(smartDate(['2016-07-04', '2016-07-05'], {includeTime: true})).to.eql('July 4th, 12:00 am - July 5th, 12:00 am')
+    expect(smartDate(['2016-07-04', '2016-07-05'])).to.eql('July 4-5')
+    expect(smartDate(['2016-07-04', '2016-07-05'], {includeYear: true})).to.eql('July 4-5, 2016')
+  })
+})
